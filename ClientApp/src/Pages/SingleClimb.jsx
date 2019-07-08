7e0387edbf2a2b10fc70abfbf879a07e0c02c5c0
dantezinfern0 <danteharasz@gmail.com>
2019-07-08 11:14:48 -0400

    changes no success

--- ClientApp/src/Pages/SingleClimb.jsx
+++ ClientApp/src/Pages/SingleClimb.jsx
@@ -3,13 +3,13 @@ import Axios from 'axios'
 
 class SingleClimb extends Component {
   state = ({
-    theClimb: theClimb
+    theClimb: {}
   })
 
   componentDidMount() {
     Axios.get(`api/climb/${this.props.match.params.Id}`).then(resp => {
-      console.log({ theClimb })
-      setState =({
+      
+      this.setState =({
         theClimb: resp.data
       })
     })
@@ -18,7 +18,7 @@ class SingleClimb extends Component {
   render() {
     return (
       <div>
-        <h1>{theClimb.routeName}</h1>
+        {/* <h1>{theClimb.routeName}</h1>
         <p>{theClimb.rating}</p>
         {theClimb.trad && <p>trad</p>}
         {theClimb.sport && <p>sport</p>}
@@ -26,7 +26,7 @@ class SingleClimb extends Component {
         <p>{theClimb.height} ft</p>
         <p>{theClimb.directions}</p>
         <p>{theClimb.description}</p>
-        <p>{theClimb.notes}</p>
+        <p>{theClimb.notes}</p> */}
       </div>
     )
   }
